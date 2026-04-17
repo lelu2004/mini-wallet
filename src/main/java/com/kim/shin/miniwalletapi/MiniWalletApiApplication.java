f75b6b96daa059ea50bb49f1043b4fb4ff6c31cb
KimShin <llelu1808@gmail.com>
2026-04-17 11:43:29 +0700

    Initial commit: Initialize the Mini e-wallet project with Spring Boot and Docker

---
+++ src/main/java/com/kim/shin/miniwalletapi/MiniWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.kim.shin.miniwalletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniWalletApiApplication.class, args);
+	}
+
+}
